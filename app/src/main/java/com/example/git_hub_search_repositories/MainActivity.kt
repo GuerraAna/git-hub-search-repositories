1c64898fa8c4d2ee8601bf72472659f41bfec3e5
GuerraAna <ana.guerrads@hotmail.com>
2023-09-23 18:40:45 -0300

    Implement viewBinding

--- app/src/main/java/com/example/git_hub_search_repositories/MainActivity.kt
+++ app/src/main/java/com/example/git_hub_search_repositories/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.git_hub_search_repositories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.git_hub_search_repositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+internal class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupBinding()
+
+        binding.textView.text = "Olá, mundo"
+    }
+
+    private fun setupBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
