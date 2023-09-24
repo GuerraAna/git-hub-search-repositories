de96165d336411526ece3603bc3e554838925dca
GuerraAna <ana.guerrads@hotmail.com>
2023-09-24 20:25:39 -0300

    feature(mock): refactor code

---
+++ app/src/main/java/com/example/git_hub_search_repositories/common/GitHubMocks.kt
@@ -0,0 +1,72 @@
+package com.example.git_hub_search_repositories.common
+
+import com.example.git_hub_search_repositories.domain.Repositories
+import com.example.git_hub_search_repositories.domain.RepositoryOwner
+
+/**
+ *
+ */
+fun gitHubRepositoriesMock(): List<Repositories> {
+	return listOf(
+			Repositories(
+					id = 647032678,
+					nodeId = "R_kgDOJpDvZg",
+					name = "abstergo-industries-aws",
+					fullName = "GuerraAna/abstergo-industries-aws",
+					private = false,
+					owner = RepositoryOwner(
+							login = "GuerraAna",
+							avatarUrl = "https://avatars.githubusercontent.com/u/78937585?v=4"
+					),
+					htmlUrl = "https://github.com/GuerraAna/abstergo-industries-aws",
+					description = "Abstergo Industries AWS",
+					fork = false,
+					url = "https://api.github.com/repos/GuerraAna/abstergo-industries-aws"
+			),
+			Repositories(
+					id = 647032678,
+					nodeId = "R_kgDOJpDvZg",
+					name = "abstergo-industries-aws",
+					fullName = "GuerraAna/abstergo-industries-aws",
+					private = false,
+					owner = RepositoryOwner(
+							login = "GuerraAna",
+							avatarUrl = "https://avatars.githubusercontent.com/u/78937585?v=4"
+					),
+					htmlUrl = "https://github.com/GuerraAna/abstergo-industries-aws",
+					description = "Abstergo Industries AWS",
+					fork = false,
+					url = "https://api.github.com/repos/GuerraAna/abstergo-industries-aws"
+			),
+			Repositories(
+					id = 647032678,
+					nodeId = "R_kgDOJpDvZg",
+					name = "abstergo-industries-aws",
+					fullName = "GuerraAna/abstergo-industries-aws",
+					private = false,
+					owner = RepositoryOwner(
+							login = "GuerraAna",
+							avatarUrl = "https://avatars.githubusercontent.com/u/78937585?v=4"
+					),
+					htmlUrl = "https://github.com/GuerraAna/abstergo-industries-aws",
+					description = "Abstergo Industries AWS",
+					fork = false,
+					url = "https://api.github.com/repos/GuerraAna/abstergo-industries-aws"
+			),
+			Repositories(
+					id = 647032678,
+					nodeId = "R_kgDOJpDvZg",
+					name = "abstergo-industries-aws",
+					fullName = "GuerraAna/abstergo-industries-aws",
+					private = false,
+					owner = RepositoryOwner(
+							login = "GuerraAna",
+							avatarUrl = "https://avatars.githubusercontent.com/u/78937585?v=4"
+					),
+					htmlUrl = "https://github.com/GuerraAna/abstergo-industries-aws",
+					description = "Abstergo Industries AWS",
+					fork = false,
+					url = "https://api.github.com/repos/GuerraAna/abstergo-industries-aws"
+			)
+	)
+}
